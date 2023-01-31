5a332e08fc8c0f28c50ca4a1704ed2a4c23745b2
reivax8560 <xavier.martin.rec@gmail.com>
2023-01-31 13:43:49 +0100

    version desktop finalisée sur chrome

--- modal.js
+++ modal.js
@@ -6,118 +6,106 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
+function dataValid(formdata) {
+  formdata.dataset.error = "";
+  formdata.dataset.errorVisible = "false";
+}
+////////////////////////////////////// DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const confirm = document.getElementById("confirm");
-
-let form = document.getElementById("formulaire");
-let firstName = document.getElementById("first");
-let blockFirstName = document.getElementById("blockFirstName");
-let lastName = document.getElementById("last");
-let blockLastName = document.getElementById("blockLastName");
-let email = document.getElementById("email");
-let blockEmail = document.getElementById("blockEmail");
-let birthDate = document.getElementById("birthdate");
-let blockBirthDate = document.getElementById("blockBirthDate");
-let quantity = document.getElementById("quantity");
-let blockQuantity = document.getElementById("blockQuantity");
-let location1 = document.getElementById("location1");
-let location2 = document.getElementById("location2");
-let location3 = document.getElementById("location3");
-let location4 = document.getElementById("location4");
-let location5 = document.getElementById("location5");
-let location6 = document.getElementById("location6");
-let blockRadio = document.getElementById("blockRadio");
-let checkbox1 = document.getElementById("checkbox1");
-let blockCheckbox = document.getElementById("blockCheckbox");
-
-/////////////////////////// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-/////////////////////////// launch modal form
-function launchModal() {
+const form = document.getElementById("formulaire");
+const closeForm = document.getElementById("closeForm");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
+const checkbox1 = document.getElementById("checkbox1");
+const confirmPage = document.getElementById("confirmpage");
+const closeConfirmpage = document.getElementById("closeConfirmpage");
+const regex1 = /^([A-Za-zàâéèëêïîôùüçœ\-\']){2,}$/;
+const regex2 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regex3 = /^([0-9]){1,2}$/;
+//////////////////////////////////////// AFFICHAGE DU FORMULAIRE
+modalBtn.forEach((btn) => btn.addEventListener("click", () => {
   modalbg.style.display = "block";
-}
+}));
 
-//////////////////////////////////////// SOUMISSION FORMULAIRE
-function dataValid(block) {
-  block.dataset.error = "";
-  block.dataset.errorVisible = "false";
-}
+// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// function launchModal() {
+//   modalbg.style.display = "block";
+// }
+
+closeForm.addEventListener('click', () => {
+  modalbg.style.display = "none";
+});
+
+//////////////////////////////////////// VALIDATION DU FORMULAIRE
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
   //////////////// VERIF PRENOM
-  if (firstName.value == "" || firstName.value.length < 2) { // ou if(!firstName.value) 
-    e.preventDefault();
-    blockFirstName.dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-    blockFirstName.dataset.errorVisible = "true";
+  if (!regex1.test(firstName.value)) {
+    formData[0].dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    formData[0].dataset.errorVisible = "true";
   } else {
-    dataValid(blockFirstName);
+    dataValid(formData[0]);
   }
   //////////////// VERIF NOM
-  if (lastName.value == "" || lastName.value.length < 2) {
-    e.preventDefault();
-    blockLastName.dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-    blockLastName.dataset.errorVisible = "true";
+  if (!regex1.test(lastName.value)) {
+    formData[1].dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    formData[1].dataset.errorVisible = "true";
   } else {
-    dataValid(blockLastName);
+    dataValid(formData[1]);
   }
   //////////////// VERIF EMAIL
-  function validateEmail(mail) {
-    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!regex.test(mail.value)) {
-      e.preventDefault();
-      blockEmail.dataset.error = "Vous devez entrer une adresse email valide.";
-      blockEmail.dataset.errorVisible = "true";
-    } else {
-      dataValid(blockEmail);
-    }
+  if (!regex2.test(email.value)) {
+    formData[2].dataset.error = "Vous devez entrer une adresse email valide.";
+    formData[2].dataset.errorVisible = "true";
+  } else {
+    dataValid(formData[2]);
   }
-  validateEmail(email);
   //////////////// VERIF NAISSANCE
   if (birthDate.value == "") {
-    e.preventDefault();
-    blockBirthDate.dataset.error = "Vous devez entrer votre date de naissance.";
-    blockBirthDate.dataset.errorVisible = "true";
+    formData[3].dataset.error = "Vous devez entrer votre date de naissance.";
+    formData[3].dataset.errorVisible = "true";
   } else {
-    dataValid(blockBirthDate);
+    dataValid(formData[3]);
   }
   //////////////// VERIF NB TOURNOIS
-  function validateQuantity(number) {
-    let regex = /^([0-9]){1,2}$/;
-    if (!regex.test(number.value)) {
-      e.preventDefault();
-      blockQuantity.dataset.error = "Vous devez entrer un nombre entre 0 et 99.";
-      blockQuantity.dataset.errorVisible = "true";
-    } else {
-      dataValid(blockQuantity);
-    }
+  if (!regex3.test(quantity.value)) {
+    formData[4].dataset.error = "Vous devez entrer un nombre entre 0 et 99.";
+    formData[4].dataset.errorVisible = "true";
+  } else {
+    dataValid(formData[4]);
   }
-  validateQuantity(quantity);
   //////////////// VERIF TOURNOI COCHÉ
   if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
-    e.preventDefault();
-    blockRadio.dataset.error = "Vous devez choisir une option.";
-    blockRadio.dataset.errorVisible = "true";
+    formData[5].dataset.error = "Vous devez choisir une option.";
+    formData[5].dataset.errorVisible = "true";
   } else {
-    dataValid(blockRadio);
+    dataValid(formData[5]);
   }
   //////////////// VERIF COND UTILISATION
   if (!checkbox1.checked) {
-    e.preventDefault();
-    blockCheckbox.dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
-    blockCheckbox.dataset.errorVisible = "true";
+    formData[6].dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
+    formData[6].dataset.errorVisible = "true";
   } else {
-    dataValid(blockCheckbox);
+    dataValid(formData[6]);
+  }
+
+  if (formData[0].dataset.errorVisible == "false" && formData[1].dataset.errorVisible == "false" && formData[2].dataset.errorVisible == "false" && formData[3].dataset.errorVisible == "false" && formData[4].dataset.errorVisible == "false" && formData[5].dataset.errorVisible == "false" && formData[6].dataset.errorVisible == "false") {
+    modalbg.style.display = "none";
+    confirmPage.style.display = "flex";
   }
 });
 
-// si formulaire valide -> div classe confirm en display "flex"
-// définir fonction validate() qui retourne "true"
-
-
-
-
-// fonctionnalité du bouton "close"
+closeConfirmpage.addEventListener('click', () => {
+  confirmPage.style.display = "none";
+});
