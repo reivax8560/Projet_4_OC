b89247925b94aa02f9482eb2fa3fe304cee7ed4e
reivax8560 <xavier.martin.rec@gmail.com>
2023-02-18 15:18:59 +0100

    version finale

--- modal.js
+++ modal.js
@@ -1,16 +1,6 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-function dataValid(formdata) {
-  formdata.dataset.error = "";
-  formdata.dataset.errorVisible = "false";
-}
-////////////////////////////////////// DOM Elements
+////////////////////////////////////////////////////////////////////// DOM Elements
+const topNav = document.getElementById("myTopnav");
+const navIcon = document.getElementById("navIcon");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
@@ -30,80 +20,93 @@ const location6 = document.getElementById("location6");
 const checkbox1 = document.getElementById("checkbox1");
 const confirmPage = document.getElementById("confirmpage");
 const closeConfirmpage = document.querySelectorAll(".closeConfirmpage");
-
-const regex1 = /^([A-Za-zàâéèëêïîôùüçœ\-\']){2,}$/;
-const regex2 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const regex3 = /^([0-9]){1,2}$/;
-//////////////////////////////////////// AFFICHAGE DU FORMULAIRE
+//////////////////////////////////////////////////////////////////////////////// REGEX
+const regex1 = /^([A-Za-zàâéèëêïîôùüçœ\-\']){2,}$/; // nom prenom
+const regex2 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // email
+const regex3 = /^([0-9]){1,2}$/; // nb tournoi
+/////////////////////////////////////////////////////////// AFFICHAGE RESPONSIVE MENU
+function editNav() {
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
+  } else {
+    topNav.className = "topnav";
+  }
+}
+navIcon.addEventListener('click', editNav);
+////////////////////////////////////////////////////////////// AFFICHAGE DU FORMULAIRE
 modalBtn.forEach((btn) => btn.addEventListener("click", () => {
   modalbg.style.display = "block";
 }));
-//////////////////////////////////////// FERMETURE DU FORMULAIRE
+////////////////////////////////////////////////////////////// FERMETURE DU FORMULAIRE
 closeForm.addEventListener('click', () => {
   modalbg.style.display = "none";
 });
-
-//////////////////////////////////////// VALIDATION DU FORMULAIRE
-form.addEventListener('submit', (e) => {
+//////////////////////////////////////////////////////////// FERMETURE PAGE DE CONFIRM
+closeConfirmpage.forEach((btn) => btn.addEventListener('click', () => {
+  confirmPage.style.display = "none";
+  modalbg.style.display = "none";
+}));
+///////////////////////////////////////////////////////// REINITIALISATION DES DATASET
+function dataValid(formdata) {
+  formdata.dataset.error = "";
+  formdata.dataset.errorVisible = "false";
+}
+///////////////////////////////////////////////////////////// VALIDATION DU FORMULAIRE
+function checkForm(e) {
   e.preventDefault();
-  //////////////// VERIF PRENOM
+  /////////////////////////////////////// VERIF PRENOM
   if (!regex1.test(firstName.value)) {
     formData[0].dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     formData[0].dataset.errorVisible = "true";
   } else {
     dataValid(formData[0]);
   }
-  //////////////// VERIF NOM
+  ////////////////////////////////////////// VERIF NOM
   if (!regex1.test(lastName.value)) {
     formData[1].dataset.error = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     formData[1].dataset.errorVisible = "true";
   } else {
     dataValid(formData[1]);
   }
-  //////////////// VERIF EMAIL
+  ///////////////////////////////////////// VERIF EMAIL
   if (!regex2.test(email.value)) {
     formData[2].dataset.error = "Vous devez entrer une adresse email valide.";
     formData[2].dataset.errorVisible = "true";
   } else {
     dataValid(formData[2]);
   }
-  //////////////// VERIF NAISSANCE
+  ///////////////////////////////////// VERIF NAISSANCE
   if (birthDate.value == "") {
     formData[3].dataset.error = "Vous devez entrer votre date de naissance.";
     formData[3].dataset.errorVisible = "true";
   } else {
     dataValid(formData[3]);
   }
-  //////////////// VERIF NB TOURNOIS
+  //////////////////////////////////// VERIF NB TOURNOIS
   if (!regex3.test(quantity.value)) {
     formData[4].dataset.error = "Vous devez entrer un nombre entre 0 et 99.";
     formData[4].dataset.errorVisible = "true";
   } else {
     dataValid(formData[4]);
   }
-  //////////////// VERIF TOURNOI COCHÉ
+  ////////////////////////////////// VERIF TOURNOI COCHÉ
   if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
     formData[5].dataset.error = "Vous devez choisir une option.";
     formData[5].dataset.errorVisible = "true";
   } else {
     dataValid(formData[5]);
   }
-  //////////////// VERIF COND UTILISATION
+  //////////////////////////////// VERIF COND UTILISATION
   if (!checkbox1.checked) {
     formData[6].dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
     formData[6].dataset.errorVisible = "true";
   } else {
     dataValid(formData[6]);
   }
-  //////////////// AFFICHAGE PAGE DE CONFIRM (SI FORMULAIRE VALIDE)
+  ////////// AFFICHAGE PAGE DE CONFIRM SI FORMULAIRE VALIDE
   if (formData[0].dataset.errorVisible == "false" && formData[1].dataset.errorVisible == "false" && formData[2].dataset.errorVisible == "false" && formData[3].dataset.errorVisible == "false" && formData[4].dataset.errorVisible == "false" && formData[5].dataset.errorVisible == "false" && formData[6].dataset.errorVisible == "false") {
     modalbg.style.display = "none";
     confirmPage.style.display = "flex";
   }
-});
-////////////////// FERMETURE PAGE DE CONFIRM
-closeConfirmpage.forEach((btn) => btn.addEventListener('click', () => {
-  confirmPage.style.display = "none";
-  modalbg.style.display = "none";
-}));
-
+}
+form.addEventListener('submit', checkForm);
